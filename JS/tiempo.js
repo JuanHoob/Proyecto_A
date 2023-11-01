770b7fbec579f1d7ee00a8304fd2f8e32c6fbcac
Andrea Katherine <jaimeandreak@gmail.com>
2023-11-01 20:46:26 +0100

    agrego primer script con parte del botón y CLAVE API, amén.

--- JS/tiempo.js
+++ JS/tiempo.js
@@ -1,54 +1,59 @@
 "use strict";
-const API_KEY = "113ab78dfa3e32fdc67b5a19d67c0d9f";
-const getLocationButton = document.getElementById("getLocation");
-const locationInfo = document.getElementById("result");
+const CLAVE_API = "113ab78dfa3e32fdc67b5a19d67c0d9f";
+const botonObtenerUbicacion = document.getElementById("obtenerUbicacion");
+const infoUbicacion = document.getElementById("resultadoBoton");
 
-getLocationButton.addEventListener("click", obtenerUbicacionYTemperatura);
+function inicializar() {
+  botonObtenerUbicacion.addEventListener("click", obtenerUbicacionYTemperatura);
+}
+
+document.addEventListener("DOMContentLoaded", inicializar);
 
 function obtenerUbicacionYTemperatura() {
   if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
+    navigator.geolocation.getCurrentPosition(function (posición) {
+      const latitud = posición.coords.latitude;
+      const longitud = posición.coords.longitude;
 
-      const openWeatherMapUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+      const urlOpenWeatherMap = `https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${CLAVE_API}`;
 
-      fetch(openWeatherMapUrl)
-        .then((response) => response.json())
-        .then((data) => {
-          const city = data.name;
-          const temperature = (data.main.temp - 273.15).toFixed(2); // Convertir a grados Celsius
+      fetch(urlOpenWeatherMap)
+        .then((respuesta) => respuesta.json())
+        .then((datos) => {
+          const ciudad = datos.name;
+          const temperatura = (datos.main.temp - 273.15).toFixed(2); // Convertir a grados Celsius
 
-          // Mostrar la ubicación y temperatura actual
-          let infoText = `Estás en ${city}, la temperatura actual es ${temperature}°C.`;
+          // Muestra la ubicación y temperatura actual
+          let textoInfo = `Estás en ${ciudad}, la temperatura actual es ${temperatura}°C.`;
 
-          // Agregar la fecha actual
-          const currentDate = new Date();
-          const formattedDate = currentDate.toLocaleDateString("es-ES", {
+          // Agrega la fecha actual
+          const fechaActual = new Date();
+          const fechaFormateada = fechaActual.toLocaleDateString("es-ES", {
             weekday: "long",
             year: "numeric",
             month: "long",
             day: "numeric",
           });
 
-          infoText += ` ${formattedDate}`;
+          textoInfo += ` ${fechaFormateada}`;
 
-          // Verificar si el pronóstico incluye lluvia
-          // (el código restante permanece sin cambios)
+          const pronosticoLluvia = true;
 
-          // Crear una lista ordenada para mostrar el pronóstico
-          // (el código restante permanece sin cambios)
+          // Para mostrar el mensaje de pronóstico de lluvia
+          const mensajeLluvia = document.getElementById("mensajeLluvia");
+          mensajeLluvia.textContent = pronosticoLluvia
+            ? "Va a llover en las próximas 8 horas"
+            : "No se esperan lluvias.";
 
-          // Agregar la lista al mensaje
-          locationInfo.textContent = infoText;
-          // (el código restante para la lista permanece sin cambios)
+          infoUbicacion.textContent = textoInfo;
         })
+
         .catch((error) => {
-          locationInfo.textContent = `Error al obtener la ubicación o la temperatura: ${error.message}`;
+          infoUbicacion.textContent = `Error al obtener la ubicación o la temperatura: ${error.message}`;
         });
     });
   } else {
-    locationInfo.textContent =
+    infoUbicacion.textContent =
       "La geolocalización no está disponible en este navegador.";
   }
 }
